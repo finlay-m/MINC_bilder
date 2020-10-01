076f487feaf2fb5d1b23ba82af8c2f92b2904026
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-10-01 13:10:40 +0200

    Added image and custom color and display different versions of image placement

--- MINC_bilder/ViewController.swift
+++ MINC_bilder/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theimageview: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // UIImage(source)
+        theimageview.image = UIImage(named: "reindeer")
+        
     }
 
 
