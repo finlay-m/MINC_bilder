d13a70211299644293e46cc4035112d323a3ec43
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-10-01 12:33:29 +0200

    Initial Commit

---
+++ MINC_bilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MINC_bilder
+//
+//  Created by Finlay MacGregor on 10/1/20.
+//  Copyright © 2020 Finlay MacGregor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
